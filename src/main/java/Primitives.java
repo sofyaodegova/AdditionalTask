ac8e48609159b9020b3b7649f6a5ce18c3531791
Sofya Odegova <sophiaodegova@mail.ru>
2022-10-29 16:23:43 +0400

    Wrappers task added

--- src/main/java/Primitives.java
+++ src/main/java/Primitives.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Primitives {
+public class Primitives {
+    public static void main(String[] args) {
+
+        byte a = 1;
+        System.out.println(a);
+
+        short b = 200;
+        System.out.println(b);
+
+        int c = 3000;
+        System.out.println(c);
+
+        long d = 13000000000L;
+        System.out.println(d);
+
+        float e = 3.14f;
+        System.out.println(e);
+
+        double f = 0.6;
+        System.out.println(f);
+
+        char g = 'G';
+        System.out.println(g);
+
+        boolean h = false;
+        System.out.println(h);
+
+    }
 }
---
+++ src/main/java/Wrappers.java
@@ -0,0 +1,28 @@
+public class Wrappers {
+    public static void main(String[] args) {
+
+        Integer i = 123;
+        System.out.println(i);
+
+        Short s = 200;
+        System.out.println(s);
+
+        Long l = 15000000000L;
+        System.out.println(l);
+
+        Byte b = 1;
+        System.out.println(b);
+
+        Float f = 3.14f;
+        System.out.println(f);
+
+        Double d = 0.56;
+        System.out.println(d);
+
+        Character c = 'A';
+        System.out.println(c);
+
+        Boolean bo = true;
+        System.out.println(bo);
+    }
+}
